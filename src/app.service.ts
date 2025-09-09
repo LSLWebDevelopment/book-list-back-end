04853a9e2a6e994a7fe248f167d753b8bd0c64a7
LSL Web Development <lslwebdevelopment@gmail.com>
2025-09-09 16:05:00 -0300

    Updated urls

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'The application is live!';
+    return 'Is the application working?';
   }
 }
