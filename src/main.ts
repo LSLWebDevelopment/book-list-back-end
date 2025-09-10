b7222402c68eefcefdc4c31eeb048afb336bf102
LSL Web Development <lslwebdevelopment@gmail.com>
2025-09-10 11:00:20 -0300

    Updated the local POST environment

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: ['http://localhost:3000', 'https://book-list-6oyc.vercel.app'],
-    methods: ['POST'],
+    methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
   });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
