1d9572e1157f12d78722eeef35dc8febb8a6f664
LSL Web Development <lslwebdevelopment@gmail.com>
2025-09-10 10:15:37 -0300

    Updated the url

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: ['http://localhost:3000', 'https://book-list-6oyc.vercel.app'],
-    methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
+    methods: ['POST'],
     allowedHeaders: ['Content-Type', 'Authorization'],
   });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
